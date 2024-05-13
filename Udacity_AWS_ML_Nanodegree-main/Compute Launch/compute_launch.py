b5db596e161a66d029f2496b4cf8d59730681278
Lala Ibadullayeva <86626835+Lala2398@users.noreply.github.com>
2024-05-13 16:05:07 +0400

    Delete Udacity_AWS_ML_Nanodegree-main directory

--- Udacity_AWS_ML_Nanodegree-main/Compute Launch/compute_launch.py
+++
@@ -1,7 +0,0 @@
-def days_until_launch(current_day, launch_day):
-    """"Returns the days left before launch.
-
-    current_day (int) - current day in integer
-    launch_day (int) - launch day in integer
-    """
-    return launch_day - current_day
